671d3aaa6587398fb55063e2e3454f418275f21b
Severino Vinicius <severino.sales95@gmail.com>
2023-09-27 18:46:12 -0300

    [Fluxo 01 - Req 01] - Implementado model Teams

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,33 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class TeamsModel extends Model<InferAttributes<TeamsModel>, InferCreationAttributes<TeamsModel>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+TeamsModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name'
+  }
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  tableName: 'teams',
+  timestamps: false,
+  underscored: true
+})
